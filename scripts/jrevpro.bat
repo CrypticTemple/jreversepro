d1ec8e2a7a0211398b846629e0ad073ca75f339e
Brian McKenna <brian@brianmckenna.org>
2011-09-03 22:30:49 +1000

    Update the jrevpro batch file

--- scripts/jrevpro.bat
+++ scripts/jrevpro.bat
@@ -2,9 +2,9 @@
 
 IF "%JREVERSE_HOME%" == "" goto envpath
 
-set CP=%JREVERSE_HOME%\jreversepro.jar;%JREVERSE_HOME%\lib\commons-cli-1.2.jar
+set CP=%JREVERSE_HOME%\target\jreversepro-1.5.0.jar;%JREVERSE_HOME%\target\dependency\commons-cli-1.2.jar;%JREVERSE_HOME%\target\dependency\commons-io-1.4.jar
  
-java -classpath %CP% net.sf.jrevpro.cmd.CommandMain %1 %2 %3 %4 %5
+java -classpath %CP% org.jreversepro.cmd.CommandMain %1 %2 %3 %4 %5
 goto end
 
 :envpath
