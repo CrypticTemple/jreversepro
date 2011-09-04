a4898d1921765b39704f679b9d2787d46c4d4483
Brian McKenna <brian@brianmckenna.org>
2011-09-04 19:08:53 +1000

    Update run scripts to use newer version of commons-io

--- scripts/jrevpro.bat
+++ scripts/jrevpro.bat
@@ -2,7 +2,7 @@
 
 IF "%JREVERSE_HOME%" == "" goto envpath
 
-set CP=%JREVERSE_HOME%\target\jreversepro-1.5.0.jar;%JREVERSE_HOME%\target\dependency\commons-cli-1.2.jar;%JREVERSE_HOME%\target\dependency\commons-io-1.4.jar
+set CP=%JREVERSE_HOME%\target\jreversepro-1.5.0.jar;%JREVERSE_HOME%\target\dependency\commons-cli-1.2.jar;%JREVERSE_HOME%\target\dependency\commons-io-2.0.1.jar
  
 java -classpath %CP% org.jreversepro.cmd.CommandMain %1 %2 %3 %4 %5
 goto end
